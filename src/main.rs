c8570a80cb9a1e5056d20489ec39a3c5c900e518
seaasses <marcusvglac@gmail.com>
2025-01-31 18:23:35 -0300

    Add initial main.rs with project description and welcome message

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("🎨 This is the Address Artisan! 🎨\n");
+    println!(
+        "✨ With this tool, you can generate vanity P2PKH addresses using only your wallet's xpub."
+    );
+    println!("🔒 This software will never know your private keys! It's safe!️\n");
+    println!("🚧 It's a work in progress project and it's not ready to use yet 🚧");
+}
