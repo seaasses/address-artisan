67b81c3663dcf288021849ae018760078f78387e
seaasses <marcusvglac@gmail.com>
2025-02-01 09:37:44 -0300

    Update CLI to use xpub instead of pubkey hash

--- src/cli.rs
+++ src/cli.rs
@@ -8,16 +8,10 @@ use clap::Parser;
 )]
 
 pub struct Cli {
-    #[arg(
-        short='P',
-        long = "pubkey-hash",
-        help = "Bitcoin public key hash (20 bytes in hex)",
-        value_parser = Cli::validate_pubkey_hash
-    )]
-    pub pubkey_hash: [u8; 20],
-
     #[arg(short = 'p', long = "prefix", help = "Prefix for the address", value_parser = Cli::validate_prefix)]
     pub prefix: String,
+    #[arg(short = 'x', long = "xpub", help = "Xpub", value_parser = Cli::validate_xpub)]
+    pub xpub: String,
 }
 
 impl Cli {
@@ -45,26 +39,21 @@ impl Cli {
         Ok(prefix.to_string())
     }
 
-    fn validate_pubkey_hash(pubkey_hash: &str) -> Result<[u8; 20], String> {
-        if pubkey_hash.is_empty() {
-            return Err("Pubkey hash cannot be empty".to_string());
+    fn validate_xpub(xpub: &str) -> Result<String, String> {
+        let valid_base58_chars = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+        if xpub.is_empty() {
+            return Err("Xpub cannot be empty".to_string());
+        }
+        if !xpub.starts_with("xpub") {
+            return Err("Xpub should start with 'xpub'.".to_string());
         }
 
-        if !pubkey_hash.chars().all(|c| c.is_ascii_hexdigit()) {
-            return Err("Pubkey hash must contain only hexadecimal characters".to_string());
+        for c in xpub.chars() {
+            if !valid_base58_chars.contains(c) {
+                return Err(format!("Invalid xpub"));
+            }
         }
 
-        if pubkey_hash.len() != 40 {
-            // 20 bytes = 40 hex characters
-            return Err("Pubkey hash must be 20 bytes (40 hex characters) long".to_string());
-        }
-
-        let mut pubkey_hash_bytes = [0u8; 20];
-        for i in 0..20 {
-            let byte_str = &pubkey_hash[i * 2..i * 2 + 2];
-            pubkey_hash_bytes[i] =
-                u8::from_str_radix(byte_str, 16).map_err(|_| "Invalid hex value".to_string())?;
-        }
-        Ok(pubkey_hash_bytes)
+        Ok(xpub.to_string())
     }
 }
