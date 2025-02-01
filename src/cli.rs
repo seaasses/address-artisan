3eeeb5679dc1c770e17c00cb05a57d871fa6d9eb
seaasses <marcusvglac@gmail.com>
2025-02-01 11:03:19 -0300

    Add max depth CLI argument with validation

--- src/cli.rs
+++ src/cli.rs
@@ -12,11 +12,31 @@ pub struct Cli {
     pub prefix: String,
     #[arg(short = 'x', long = "xpub", help = "Xpub", value_parser = Cli::validate_xpub)]
     pub xpub: String,
+    #[arg(
+        short = 'm',
+        long = "max-depth",
+        help = "Max depth for the path's last index",
+        default_value = "1000",
+        value_parser = Cli::validate_max_depth
+    )]
+    pub max_depth: u32,
 }
 
 impl Cli {
     pub fn parse_args() -> Self {
         Self::parse()
+    }
+
+    fn validate_max_depth(max_depth: &str) -> Result<u32, String> {
+        let max_depth: u32 = max_depth
+            .parse()
+            .map_err(|e: std::num::ParseIntError| e.to_string())?;
+
+        if max_depth > 0x7FFFFFFF {
+            return Err("Max depth must be less than 2^31 - 1".to_string());
+        }
+
+        Ok(max_depth)
     }
 
     fn validate_prefix(prefix: &str) -> Result<String, String> {
