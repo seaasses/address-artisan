c47f59d196161002b4366f95c85e6a73fa2207f8
seaasses <marcusvglac@gmail.com>
2025-02-01 11:03:37 -0300

    Randomize initial derivation path and pass max depth to VanityAddressFinder

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,17 @@ mod cli;
 mod prefix_validator;
 mod vanity_address_finder;
 mod xpub;
+use rand;
 
 fn main() {
     let args = cli::Cli::parse_args();
     let xpub = args.xpub;
     let prefix = args.prefix;
-    let initial_path = vec![0];
+    let max_depth = args.max_depth;
 
+    let initial_path = vec![rand::random::<u32>() & 0x7FFFFFFF];
     let address_finder =
-        vanity_address_finder::VanityAddressFinder::new(prefix, xpub, initial_path);
+        vanity_address_finder::VanityAddressFinder::new(prefix, xpub, initial_path, max_depth);
 
     match address_finder.find_address() {
         Ok((address, path)) => println!("Found address: {}. Path: xpub/{}", address, path),
