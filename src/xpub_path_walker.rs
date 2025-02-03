6ea581a8020bb6344024442228685af93dc48829
seaasses <marcusvglac@gmail.com>
2025-02-02 21:27:29 -0300

    Modify XpubPathWalker to generate pubkey hashes from xpub

--- src/xpub_path_walker.rs
+++ src/xpub_path_walker.rs
@@ -1,50 +1,62 @@
-pub struct XpubPathWalker {
+use crate::xpub::ExtendedPublicKeyDeriver;
+
+pub struct XpubPubkeyHashWalker {
     xpub_path: Vec<u32>,
     max_depth: u32,
     first_call: bool,
+    xpub_public_key_deriver: ExtendedPublicKeyDeriver,
 }
 
-const NON_HARDENING_MAX_INDEX: u32 = 0x7FFFFFFF;
-
-impl Iterator for XpubPathWalker {
-    type Item = Vec<u32>;
+impl Iterator for XpubPubkeyHashWalker {
+    type Item = [u8; 20];
 
     fn next(&mut self) -> Option<Self::Item> {
-        Some(self.next_path())
+        self.next_pubkey_hash()
     }
 }
 
-impl XpubPathWalker {
-    pub fn new(initial_path: Vec<u32>, max_depth: u32) -> Self {
+impl XpubPubkeyHashWalker {
+    pub fn new(xpub: String, initial_path: Vec<u32>, max_depth: u32) -> Self {
         let xpub_path = [initial_path, vec![0, 0, 0]].concat();
+        let xpub_public_key_deriver = ExtendedPublicKeyDeriver::new(&xpub);
         Self {
             xpub_path,
             max_depth,
             first_call: true,
+            xpub_public_key_deriver,
         }
     }
 
-    fn next_path(&mut self) -> Vec<u32> {
+    fn next_pubkey_hash(&mut self) -> Option<[u8; 20]> {
+        self.next_path();
+        let pubkey_hash = self
+            .xpub_public_key_deriver
+            .get_pubkey_hash_160(&self.xpub_path);
+        if pubkey_hash.is_err() {
+            return None;
+        }
+        Some(pubkey_hash.unwrap())
+    }
+
+    fn next_path(&mut self) {
         if self.first_call {
             self.first_call = false;
-            return self.xpub_path.clone();
+            return;
         }
 
         let mut last_index = self.xpub_path.len() - 1;
         if self.xpub_path[last_index] < self.max_depth {
             self.xpub_path[last_index] += 1;
-            return self.xpub_path.clone();
         }
 
         self.xpub_path.truncate(last_index - 1);
         last_index = self.xpub_path.len() - 1;
 
-        if self.xpub_path[last_index] < NON_HARDENING_MAX_INDEX {
+        if self.xpub_path[last_index] < self.xpub_public_key_deriver.non_hardening_max_index {
             self.xpub_path[last_index] += 1;
             self.xpub_path.extend_from_slice(&[0, 0]);
         } else {
             self.xpub_path.extend_from_slice(&[0, 0, 0]);
         }
-        self.xpub_path.clone()
     }
 }
