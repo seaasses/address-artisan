897c13cb13c7eacca46322c4e8e9fb7117104154
seaasses <marcusvglac@gmail.com>
2025-02-05 11:24:00 -0300

    pubkey path walker returns n paths instead of iterator

--- src/extended_public_key_path_walker.rs
+++ src/extended_public_key_path_walker.rs
@@ -1,20 +1,11 @@
-pub struct ExtendedPUblicKeyPathWalker {
+pub struct ExtendedPublicKeyPathWalker {
     xpub_path: Vec<u32>,
     max_depth: u32,
     first_call: bool,
     max_non_hardening_index: u32,
 }
 
-impl Iterator for ExtendedPUblicKeyPathWalker {
-    type Item = Vec<u32>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.next_path();
-        return Some(self.xpub_path.clone());
-    }
-}
-
-impl ExtendedPUblicKeyPathWalker {
+impl ExtendedPublicKeyPathWalker {
     pub fn new(initial_path: Vec<u32>, max_depth: u32) -> Self {
         for derivation in initial_path.clone() {
             if derivation > 0x7FFFFFFF {
@@ -29,26 +20,30 @@ impl ExtendedPUblicKeyPathWalker {
             max_non_hardening_index: 0x7FFFFFFF,
         }
     }
-
-    fn next_path(&mut self) {
+    pub fn get_n_next_paths(&mut self, n: usize) -> Vec<Vec<u32>> {
+        let mut paths = Vec::new();
+        let initial_path: Vec<u32> = self.xpub_path.clone();
         if self.first_call {
+            paths.push(initial_path);
             self.first_call = false;
-            return;
         }
-
-        let mut last_index = self.xpub_path.len() - 1;
-        if self.xpub_path[last_index] < self.max_depth {
-            self.xpub_path[last_index] += 1;
-        } else {
-            self.xpub_path.truncate(last_index - 1);
-            last_index = self.xpub_path.len() - 1;
-
-            if self.xpub_path[last_index] < self.max_non_hardening_index {
+        for _ in 0..(n - paths.len()) {
+            let mut last_index = self.xpub_path.len() - 1;
+            if self.xpub_path[last_index] < self.max_depth {
                 self.xpub_path[last_index] += 1;
-                self.xpub_path.extend_from_slice(&[0, 0]);
             } else {
-                self.xpub_path.extend_from_slice(&[0, 0, 0]);
+                self.xpub_path.truncate(last_index - 1);
+                last_index = self.xpub_path.len() - 1;
+
+                if self.xpub_path[last_index] < self.max_non_hardening_index {
+                    self.xpub_path[last_index] += 1;
+                    self.xpub_path.extend_from_slice(&[0, 0]);
+                } else {
+                    self.xpub_path.extend_from_slice(&[0, 0, 0]);
+                }
             }
+            paths.push(self.xpub_path.clone());
         }
+        paths
     }
 }
