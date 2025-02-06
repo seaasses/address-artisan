0d47fb13afceda69fed705d90d6d481e6cb677b4
seaasses <marcusvglac@gmail.com>
2025-02-06 16:49:08 -0300

    update logger

--- src/logger.rs
+++ src/logger.rs
@@ -1,10 +1,11 @@
+use std::io::{self, Write};
 use std::thread;
 use std::time::Duration;
-use std::io::{self, Write};
 
 pub struct Logger {
     serious_mode: bool,
-    pub first_log_status: bool,
+    first_log_status: bool,
+    blocked: bool,
 }
 
 impl Logger {
@@ -12,6 +13,7 @@ impl Logger {
         Logger {
             serious_mode,
             first_log_status: true,
+            blocked: false,
         }
     }
 
@@ -31,7 +33,7 @@ impl Logger {
             print!("Waiting {} seconds to reach initial hashrate", wait_time);
         } else {
             print!(
-                "👨‍🎨: Just wait here for {} seconds, I will prepare the studio",
+                "👨‍🎨: Just wait here for {} seconds, I will prepare my studio",
                 wait_time
             );
         }
@@ -49,6 +51,9 @@ impl Logger {
         }
     }
     pub fn log_status(&mut self, generated: usize, found: usize, run_time: f64, hashrate: f64) {
+        if self.blocked {
+            return;
+        }
         if self.first_log_status {
             self.first_log_status = false;
             println!("");
@@ -56,7 +61,10 @@ impl Logger {
 
         if self.serious_mode {
             self.erase_previous_line();
-            println!("[{:.2} add/s] {} in {:.1} seconds", hashrate, generated, run_time);
+            println!(
+                "[{:.2} add/s] {} in {:.1} seconds",
+                hashrate, generated, run_time
+            );
         } else {
             self.erase_previous_line();
             if found == 0 {
@@ -66,10 +74,36 @@ impl Logger {
                 );
             } else {
                 println!(
-                    "I already built {} addresses in the last {} seconds. Wow, that is {} per second! I found {} addresses!",
+                    "👨‍🎨: I already built {} addresses in the last {} seconds. Wow, that is {} per second! I found {} addresses!",
                     generated, run_time, hashrate, found
                 );
             }
+        }
+    }
+    pub fn log_found_address(&mut self, address: &str, path: &[u32]) {
+        self.blocked = true;
+        let address_index = path.last().unwrap();
+        let without_last_two = path
+            .iter()
+            .take(path.len() - 2)
+            .copied()
+            .collect::<Vec<u32>>();
+        let path_str = without_last_two
+            .iter()
+            .map(|p| p.to_string())
+            .collect::<Vec<String>>()
+            .join("/");
+
+        if self.serious_mode {
+            println!(
+                "Found address: {} at path xpub'/{}, receive address {}",
+                address, path_str, address_index
+            );
+        } else {
+            println!(
+                "👨‍🎨: Eureka! {} at path xpub'/{}, receive address {}",
+                address, path_str, address_index
+            );
         }
     }
     fn erase_previous_line(&self) {
