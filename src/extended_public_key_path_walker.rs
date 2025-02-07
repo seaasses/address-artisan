5aefe58a094ad20aaa7cd99554bf1c3f7bb4f7b1
seaasses <marcusvglac@gmail.com>
2025-02-07 16:11:09 -0300

    key path walker as an iterator

--- src/extended_public_key_path_walker.rs
+++ src/extended_public_key_path_walker.rs
@@ -20,30 +20,32 @@ impl ExtendedPublicKeyPathWalker {
             max_non_hardening_index: 0x7FFFFFFF,
         }
     }
-    pub fn get_n_next_paths(&mut self, n: usize) -> Vec<Vec<u32>> {
-        let mut paths = Vec::new();
+}
+
+impl Iterator for ExtendedPublicKeyPathWalker {
+    type Item = Vec<u32>;
+
+    fn next(&mut self) -> Option<Self::Item> {
         if self.first_call {
-            paths.push(self.xpub_path.clone());
             self.first_call = false;
+            return Some(self.xpub_path.clone());
         }
-        for _ in 0..(n - paths.len()) {
-            let mut last_index = self.xpub_path.len() - 1;
-            if self.xpub_path[last_index] < self.max_depth {
-                self.xpub_path[last_index] += 1;
+        
+        let mut last_index = self.xpub_path.len() - 1;
+        if self.xpub_path[last_index] < self.max_depth {
+            self.xpub_path[last_index] += 1;
+        } else {
+            if self.xpub_path[last_index - 2] < self.max_non_hardening_index {
+                // [x, y, non_max_hardening, 0, max_index] -> [x, y, non_max_hardening+1, 0, 0]
+                self.xpub_path[last_index - 2] += 1;
+                self.xpub_path[last_index] = 0;
             } else {
-                if self.xpub_path[last_index - 2] < self.max_non_hardening_index {
-                    // [x, y, non_max_hardening, 0, max_index] -> [x, y, non_max_hardening+1, 0, 0]
-                    self.xpub_path[last_index - 2] += 1;
-                    self.xpub_path[last_index] = 0;
-                } else {
-                    last_index += 1;
-                    // [x, y, max_hardening, 0, max_index] -> [x, y, max_hardening, 0, 0, 0 (new)]
-                    self.xpub_path.push(0);
-                    self.xpub_path[last_index - 1] = 0;
-                }
+                last_index += 1;
+                // [x, y, max_hardening, 0, max_index] -> [x, y, max_hardening, 0, 0, 0 (new)]
+                self.xpub_path.push(0);
+                self.xpub_path[last_index - 1] = 0;
             }
-            paths.push(self.xpub_path.clone());
         }
-        paths
+        Some(self.xpub_path.clone())
     }
 }
