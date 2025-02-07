949ae0614aab2a61fbcf948242227e0175711521
seaasses <marcusvglac@gmail.com>
2025-02-07 17:25:13 -0300

    add tests to bitcoin address helper

--- src/bitcoin_address_helper.rs
+++ src/bitcoin_address_helper.rs
@@ -33,20 +33,82 @@ impl BitcoinAddressHelper {
     }
 
     pub fn get_pubkey_hash_from_address(&self, address: String) -> Option<[u8; 20]> {
-        let address_bytes = bs58::decode(address).into_vec().unwrap();
+        let address_bytes = bs58::decode(address).into_vec().ok()?;
+
         if address_bytes.len() != 25 {
             return None;
         }
-        let pubkey_hash = address_bytes
-            .iter()
-            .skip(1)
-            .take(20)
-            .cloned()
-            .collect::<Vec<u8>>();
-        Some(pubkey_hash.try_into().unwrap())
+
+        address_bytes[1..21].try_into().ok()
     }
 
     fn base58_encode(&self, data: &[u8]) -> String {
         bs58::encode(data).into_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_address_from_pubkey_hash() {
+        let helper = BitcoinAddressHelper::new();
+        let pubkey_hash = [
+            0x62, 0x31, 0x50, 0x63, 0x75, 0xbc, 0x46, 0x62, 0x98, 0x2d, 0x3e, 0x08, 0x5e, 0x67,
+            0x5f, 0x55, 0x7c, 0xc1, 0x99, 0xf4,
+        ];
+
+        let address = helper.get_address_from_pubkey_hash(pubkey_hash);
+        assert_eq!(address, "19xCJCT3D55J6JcpAmbtDrSjiT1D2pQiPj");
+    }
+
+    #[test]
+    fn test_get_address_with_fake_checksum() {
+        let helper = BitcoinAddressHelper::new();
+        let pubkey_hash = [
+            0x62, 0x31, 0x50, 0x63, 0x75, 0xbc, 0x46, 0x62, 0x98, 0x2d, 0x3e, 0x08, 0x5e, 0x67,
+            0x5f, 0x55, 0x7c, 0xc1, 0x99, 0xf4,
+        ];
+        let address = helper.get_address_with_fake_checksum(pubkey_hash);
+
+        // with fake checksum is different from real one, but the init of the address is the samer
+        assert_eq!(address, "19xCJCT3D55J6JcpAmbtDrSjiT1CzUPtdm");
+    }
+
+    #[test]
+    fn test_get_pubkey_hash_from_address() {
+        let helper = BitcoinAddressHelper::new();
+        let address = "19xCJCT3D55J6JcpAmbtDrSjiT1D2pQiPj".to_string();
+        let pubkey_hash = helper.get_pubkey_hash_from_address(address).unwrap();
+        println!("Pubkey hash: {:?}", pubkey_hash);
+        assert_eq!(
+            pubkey_hash,
+            [
+                0x62, 0x31, 0x50, 0x63, 0x75, 0xbc, 0x46, 0x62, 0x98, 0x2d, 0x3e, 0x08, 0x5e, 0x67,
+                0x5f, 0x55, 0x7c, 0xc1, 0x99, 0xf4,
+            ]
+        );
+    }
+
+    #[test]
+    fn test_get_pubkey_hash_from_invalid_address() {
+        let helper = BitcoinAddressHelper::new();
+        let invalid_address = "invalid_address".to_string();
+        assert!(helper
+            .get_pubkey_hash_from_address(invalid_address)
+            .is_none());
+    }
+
+    #[test]
+    fn test_roundtrip() {
+        let helper = BitcoinAddressHelper::new();
+        let original_pubkey_hash = [
+            0x62, 0x31, 0x50, 0x63, 0x75, 0xbc, 0x46, 0x62, 0x98, 0x2d, 0x3e, 0x08, 0x5e, 0x67,
+            0x5f, 0x55, 0x7c, 0xc1, 0x99, 0xf4,
+        ];
+        let address = helper.get_address_from_pubkey_hash(original_pubkey_hash);
+        let recovered_pubkey_hash = helper.get_pubkey_hash_from_address(address).unwrap();
+        assert_eq!(original_pubkey_hash, recovered_pubkey_hash);
     }
 }
