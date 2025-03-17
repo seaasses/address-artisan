a1c06f772105a8750042c1375e25c142b58adf9b
seaasses <marcusvglac@gmail.com>
2025-03-17 16:33:34 -0300

    just run tests

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
-mod ocl_test_class;
+mod test_cl_sha256;
+mod test_cl_sha512;
+mod test_uint256_t_operations;
+mod test_secp256k1_operations;
 
-use ocl_test_class::OclTestClass;
-use std::time::Instant;
 fn main() {
-    let mut ocl_test_class = OclTestClass::new().unwrap();
-    // let a: usize = 2_usize.pow(50);
-    let mut start = Instant::now();
-    let a: u32 = 600_000_000; // max 4 billion
-    for i in 0..20 {
-        let new_quant = a + i;
-        match ocl_test_class.run(new_quant) {
-            Ok(_) => {
-                println!("Time taken: {:?}", start.elapsed());
-                start = Instant::now();
-            }
-            Err(_) => (),
-        };
-    }
 }
