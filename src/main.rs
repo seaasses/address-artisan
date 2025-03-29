d068532ed8dc0d3a37b20b0a8aa32e303ef0e955
seaasses <marcusvglac@gmail.com>
2025-03-29 14:42:26 -0300

    add tests to main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod test_cl_sha256;
 mod test_cl_sha512;
-mod test_uint256_t_operations;
-mod test_modular_operations;
+mod test_cl_uint256_t_operations;
+mod test_cl_modular_operations;
 mod test_cl_sha512_for_165_bytes_message;
+mod test_cl_secp256k1_operations;
 
 fn main() {
 }
