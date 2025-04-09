43a3f0baea44897692235e4384df7d566211957b
seaasses <marcusvglac@gmail.com>
2025-04-08 21:57:57 -0300

    fix header

--- src/opencl/headers/uint256/subtractionWithUnderflowFlag.h
+++ src/opencl/headers/uint256/subtractionWithUnderflowFlag.h
@@ -1,4 +1,4 @@
 #include "src/opencl/structs/structs.h"
 
 void uint256SubtractionWithUnderflowFlag(const UInt256 *a, const UInt256 *b,
-                                         UInt256 *result, bool *underflowFlag);
+                                         UInt256 *result, unsigned int *underflowFlag);
