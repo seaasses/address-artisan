e761aba420116b56f0878415fff33f8fbd6e6dfc
seaasses <marcusvglac@gmail.com>
2025-05-04 19:02:30 -0300

    separate from and to bytes

---
+++ src/opencl/headers/big_uint/uint256_from_bytes.h
@@ -0,0 +1,3 @@
+#include "src/opencl/structs/structs.h"
+
+Uint256 uint256_from_bytes(const unsigned char *input);
---
+++ src/opencl/headers/big_uint/uint256_to_bytes.h
@@ -0,0 +1,3 @@
+#include "src/opencl/structs/structs.h"
+
+void uint256_to_bytes(const Uint256 a, unsigned char *result);
