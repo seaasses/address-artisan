7050c0c62f14a83f2603f804eadeb064a9ab7041
seaasses <marcusvglac@gmail.com>
2025-09-22 19:54:35 -0300

    add uint256 subtraction with underflow flag

--- src/opencl/headers/big_uint/big_uint_subtraction.h
+++ src/opencl/headers/big_uint/big_uint_subtraction.h
@@ -1,3 +1,6 @@
 #include "src/opencl/structs/structs.h"
 
 void uint256_subtraction(const Uint256 *a, const Uint256 *b, Uint256 *result);
+
+void uint256_subtraction_with_underflow_flag(const Uint256 *a, const Uint256 *b,
+                                             Uint256 *result, unsigned int *underflowFlag);
---
+++ src/opencl/kernels/uint256_subtraction_with_underflow_flag_kernel.c
@@ -0,0 +1,38 @@
+#include "src/opencl/headers/big_uint/big_uint_subtraction.h"
+#include "src/opencl/headers/big_uint/big_uint_from_bytes.h"
+#include "src/opencl/headers/big_uint/big_uint_to_bytes.h"
+
+__kernel void uint256_subtraction_with_underflow_flag_kernel(
+    __global unsigned char *input_a,
+    __global unsigned char *input_b,
+    __global unsigned char *result,
+    __global unsigned int *underflow_flag)
+{
+
+    unsigned char local_a[32];
+    unsigned char local_b[32];
+    unsigned char local_result[32];
+
+    for (unsigned char i = 0; i < 32; i++)
+    {
+        local_a[i] = input_a[i];
+        local_b[i] = input_b[i];
+    }
+
+    const Uint256 a = uint256_from_bytes(local_a);
+    const Uint256 b = uint256_from_bytes(local_b);
+
+    Uint256 local_class_result;
+    unsigned int local_underflow_flag;
+
+    uint256_subtraction_with_underflow_flag(&a, &b, &local_class_result, &local_underflow_flag);
+
+    uint256_to_bytes(local_class_result, local_result);
+
+    for (unsigned char i = 0; i < 32; i++)
+    {
+        result[i] = local_result[i];
+    }
+
+    *underflow_flag = local_underflow_flag;
+}
