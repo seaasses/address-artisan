f1bcc1f36146334d56b064d0fc4f500ffbca38c8
seaasses <marcusvglac@gmail.com>
2025-09-23 00:05:39 -0300

    add points

--- src/opencl/structs/structs.h
+++ src/opencl/structs/structs.h
@@ -12,3 +12,16 @@ typedef struct
 {
   unsigned long limbs[8];
 } Uint512;
+
+typedef struct
+{
+  Uint256 x;
+  Uint256 y;
+} Point;
+
+typedef struct
+{
+  Uint256 x;
+  Uint256 y;
+  Uint256 z;
+} JacobianPoint;
