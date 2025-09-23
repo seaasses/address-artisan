24355ad1b8a02a3b8126c3897c92431bb434452c
seaasses <marcusvglac@gmail.com>
2025-09-23 01:12:59 -0300

    add jacobian point affine point addition

---
+++ src/opencl/kernels/jacobian_point_affine_point_addition_kernel.c
@@ -0,0 +1,57 @@
+#include "src/opencl/headers/secp256k1/jacobian_point_affine_point_addition.h"
+#include "src/opencl/headers/big_uint/big_uint_from_bytes.h"
+#include "src/opencl/headers/big_uint/big_uint_to_bytes.h"
+
+__kernel void jacobian_point_affine_point_addition_kernel(
+    __global unsigned char *jac_a_x_buffer,
+    __global unsigned char *jac_a_y_buffer,
+    __global unsigned char *jac_a_z_buffer,
+    __global unsigned char *aff_b_x_buffer,
+    __global unsigned char *aff_b_y_buffer,
+    __global unsigned char *result_x_buffer,
+    __global unsigned char *result_y_buffer,
+    __global unsigned char *result_z_buffer)
+{
+    JacobianPoint jac_a;
+    Point aff_b;
+    JacobianPoint result;
+
+    // Copy data from global to private memory and convert
+    unsigned char jac_a_x_private[32];
+    unsigned char jac_a_y_private[32];
+    unsigned char jac_a_z_private[32];
+    unsigned char aff_b_x_private[32];
+    unsigned char aff_b_y_private[32];
+
+    for (int i = 0; i < 32; i++) {
+        jac_a_x_private[i] = jac_a_x_buffer[i];
+        jac_a_y_private[i] = jac_a_y_buffer[i];
+        jac_a_z_private[i] = jac_a_z_buffer[i];
+        aff_b_x_private[i] = aff_b_x_buffer[i];
+        aff_b_y_private[i] = aff_b_y_buffer[i];
+    }
+
+    // Convert byte arrays to Uint256
+    bytes_to_uint256(jac_a_x_private, &jac_a.x);
+    bytes_to_uint256(jac_a_y_private, &jac_a.y);
+    bytes_to_uint256(jac_a_z_private, &jac_a.z);
+    bytes_to_uint256(aff_b_x_private, &aff_b.x);
+    bytes_to_uint256(aff_b_y_private, &aff_b.y);
+
+    // Perform jacobian point + affine point addition
+    jacobian_point_affine_point_addition(&jac_a, &aff_b, &result);
+
+    // Convert result back to bytes and copy to global memory
+    unsigned char result_x_private[32];
+    unsigned char result_y_private[32];
+    unsigned char result_z_private[32];
+    uint256_to_bytes(result.x, result_x_private);
+    uint256_to_bytes(result.y, result_y_private);
+    uint256_to_bytes(result.z, result_z_private);
+
+    for (int i = 0; i < 32; i++) {
+        result_x_buffer[i] = result_x_private[i];
+        result_y_buffer[i] = result_y_private[i];
+        result_z_buffer[i] = result_z_private[i];
+    }
+}
--- src/opencl/tests/secp256k1/mod.rs
+++ src/opencl/tests/secp256k1/mod.rs
@@ -1,1 +1,2 @@
 pub mod test_jacobian_to_affine;
+pub mod test_jacobian_point_affine_point_addition;
