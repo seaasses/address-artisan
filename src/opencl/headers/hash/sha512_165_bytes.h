09883bc541b76703250042e30d64b807b8da7999
seaasses <marcusvglac@gmail.com>
2025-10-27 17:54:52 -0300

    feat: add sha512_192

--- src/opencl/headers/hash/sha512_165_bytes.h
+++
@@ -1,6 +0,0 @@
-#include "src/opencl/structs/structs.h"
-
-#define SHA512_165_BYTES_MESSAGE_SIZE 165
-#define SHA512_HASH_SIZE 64
-
-void sha512_165_bytes(const unsigned char *restrict message, unsigned char *restrict hash);
--- src/opencl/tests/hash/mod.rs
+++ src/opencl/tests/hash/mod.rs
@@ -1,1 +1,2 @@
 pub mod test_sha512_165_bytes;
+pub mod test_sha512_192_bytes;
