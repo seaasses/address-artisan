57276b44ebaf35522df356b079176220b6a56fe1
seaasses <marcusvglac@gmail.com>
2025-11-07 16:27:33 -0300

    refact: refact main

--- src/main.rs
+++ src/main.rs
@@ -1,186 +1,64 @@
-mod bench_config;
 mod bitcoin_address_helper;
 mod cli;
-mod cpu_working_bench;
+mod constants;
+mod cpu_workbench;
+mod device_info;
+mod device_manager;
+mod events;
 mod extended_public_key;
 mod extended_public_key_deriver;
 mod extended_public_key_path_walker;
 mod logger;
+mod orchestrator;
 mod prefix;
-mod state_handler;
-mod vanity_address;
-mod working_bench;
+mod workbench;
+mod workbench_config;
+mod workbench_factory;
 
 use cli::Cli;
+use device_manager::DeviceManager;
 use extended_public_key::ExtendedPubKey;
-use extended_public_key_deriver::ExtendedPublicKeyDeriver;
-use extended_public_key_path_walker::ExtendedPublicKeyPathWalker;
 use logger::Logger;
-use rand;
-use state_handler::StateHandler;
-use std::io::{self, Write};
-use std::sync::atomic::{AtomicBool, AtomicUsize};
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::time::Duration;
-use vanity_address::VanityAddress;
-
-const STATUS_UPDATE_INTERVAL: Duration = Duration::from_secs(3);
-const THREADS_BATCH_SIZE: usize = 10000;
-const WAIT_TIME_FOR_INITIAL_HASHRATE: u8 = 30;
-
-fn setup_worker_thread(
-    xpub: ExtendedPubKey,
-    prefix: String,
-    max_depth: u32,
-    num_threads: u32,
-    global_generated_counter: Arc<AtomicUsize>,
-    global_found_counter: Arc<AtomicUsize>,
-    running: Arc<AtomicBool>,
-    found_addresses: Arc<Mutex<Vec<(String, Vec<u32>)>>>,
-) {
-    let mut handles = vec![];
-
-    for _ in 0..num_threads {
-        let xpub = xpub.clone();
-        let prefix = prefix.clone();
-        let max_depth = max_depth;
-        let global_generated_counter = global_generated_counter.clone();
-        let global_found_counter = global_found_counter.clone();
-        let running = running.clone();
-        let found_addresses = found_addresses.clone();
-
-        let handle = thread::spawn(move || {
-            let initial_path = vec![rand::random::<u32>() & 0x7FFFFFFF];
-            let xpub_path_walker = ExtendedPublicKeyPathWalker::new(initial_path, max_depth);
-            let mut xpub_deriver = ExtendedPublicKeyDeriver::new(&xpub);
-            let vanity_address = VanityAddress::new(&prefix);
-            let mut state_handler = StateHandler::new(
-                Arc::clone(&global_generated_counter),
-                Arc::clone(&global_found_counter),
-                running,
-                THREADS_BATCH_SIZE,
-                Arc::clone(&found_addresses),
-            );
-
-            for path in xpub_path_walker {
-                if !state_handler.is_running() {
-                    break;
-                }
-                if let Ok(pubkey_hash) = xpub_deriver.get_pubkey_hash_160(&path) {
-                    if let Some(address) = vanity_address.get_vanity_address(pubkey_hash) {
-                        state_handler.add_found_address(address, path);
-                    }
-                    state_handler.new_generated();
-                }
-            }
-        });
-        handles.push(handle);
-    }
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-}
-
-fn setup_logger_thread(
-    global_generated_counter: Arc<AtomicUsize>,
-    global_found_counter: Arc<AtomicUsize>,
-    running: Arc<AtomicBool>,
-    prefix: String,
-    found_addresses: Arc<Mutex<Vec<(String, Vec<u32>)>>>,
-    serious_mode: bool,
-) -> thread::JoinHandle<()> {
-    thread::spawn(move || {
-        let mut logger = Logger::new(serious_mode);
-
-        let state_handler = StateHandler::new(
-            Arc::clone(&global_generated_counter),
-            Arc::clone(&global_found_counter),
-            running,
-            THREADS_BATCH_SIZE,
-            Arc::clone(&found_addresses),
-        );
-
-        if !state_handler.is_running() {
-            return;
-        }
-        logger.start(prefix);
-
-        if !state_handler.is_running() {
-            let found_addresses = state_handler.get_found_addresses();
-            for (address, xpath_path) in found_addresses {
-                logger.log_found_address(&address, &xpath_path);
-            }
-            return;
-        }
-        logger.wait_for_hashrate(WAIT_TIME_FOR_INITIAL_HASHRATE);
-
-        thread::sleep(Duration::from_secs(2));
-        for _ in 0..WAIT_TIME_FOR_INITIAL_HASHRATE {
-            thread::sleep(Duration::from_secs(1));
-            if !state_handler.is_running() {
-                break;
-            }
-            print!(".");
-            io::stdout().flush().unwrap();
-        }
-        println!();
-
-        if state_handler.is_running() {
-            let hashrate = state_handler.get_hashrate();
-            logger.print_statistics(hashrate);
-        }
-
-        while state_handler.is_running() {
-            let (generated, found, run_time, hashrate) = state_handler.get_statistics();
-            logger.log_status(generated, found, run_time, hashrate);
-
-            thread::sleep(STATUS_UPDATE_INTERVAL);
-        }
-
-        if state_handler.get_found() > 0 {
-            let found_addresses = state_handler.get_found_addresses();
-            for (address, xpath_path) in found_addresses {
-                logger.log_found_address(&address, &xpath_path);
-            }
-        }
-    })
-}
+use orchestrator::Orchestrator;
+use prefix::Prefix;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 
 fn main() {
     let cli = Cli::parse_args();
 
-    let global_generated_counter = Arc::new(AtomicUsize::new(0));
-    let global_found_counter = Arc::new(AtomicUsize::new(0));
-    let running = Arc::new(AtomicBool::new(true));
-    let found_addresses = Arc::new(Mutex::new(Vec::new()));
+    let prefix = Prefix::new(&cli.prefix);
+    let xpub = ExtendedPubKey::from_str(&cli.xpub).unwrap();
 
-    let num_threads = thread::available_parallelism()
-        .map(|p| p.get() as u32)
-        .unwrap_or(4);
+    let stop_signal = Arc::new(AtomicBool::new(false));
+    let stop_signal_clone = Arc::clone(&stop_signal);
 
-    let logger_handle = setup_logger_thread(
-        Arc::clone(&global_generated_counter),
-        Arc::clone(&global_found_counter),
-        Arc::clone(&running),
-        cli.prefix.clone(),
-        Arc::clone(&found_addresses),
-        !cli.i_am_boring,
-    );
+    let logger_for_ctrlc = Logger::new();
+    ctrlc::set_handler(move || {
+        logger_for_ctrlc.stop_requested();
+        stop_signal_clone.store(true, Ordering::Relaxed);
+    })
+    .expect("Error setting Ctrl+C handler");
 
-    let xpub_class = ExtendedPubKey::from_str(&cli.xpub).unwrap();
+    let mut devices = DeviceManager::detect_available_devices();
 
-    setup_worker_thread(
-        xpub_class,
-        cli.prefix,
-        cli.max_depth,
-        num_threads,
-        global_generated_counter,
-        global_found_counter,
-        running,
-        found_addresses,
-    );
+    if cli.cpu_threads != 0 {
+        devices = devices
+            .into_iter()
+            .map(|device| device.with_threads(cli.cpu_threads))
+            .collect();
+    }
 
-    logger_handle.join().unwrap();
+    let actual_threads = devices
+        .first()
+        .and_then(|d| d.threads())
+        .unwrap_or(cli.cpu_threads);
+
+    let logger = Logger::new();
+    logger.start(&cli.prefix, cli.max_depth, actual_threads);
+    println!();
+
+    let mut orchestrator = Orchestrator::new(xpub, prefix, cli.max_depth, stop_signal, logger);
+
+    orchestrator.run(devices);
 }
